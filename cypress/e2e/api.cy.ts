aab331891662983472f9ef09718c6d32495a4254
Anton Tratseuski <anton.tratseuski@innowise.com>
2026-02-21 12:58:44 +0300

    First test

---
+++ cypress/e2e/api.cy.ts
@@ -0,0 +1,8 @@
+describe('Titanic API K8s Tests', () => {
+  it('Gateway Health Check returns ok', () => {
+    // Делаем запрос к нашему Gateway, который прокинут на порт 8000
+    cy.request('http://localhost:8000/health').then((response) => {
+      expect(response.status).to.eq(200);
+    });
+  });
+});
